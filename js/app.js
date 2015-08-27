71a2218659ad25191cc46647822621710330cb0e
Garan Guillory <garanguillory@gmail.com>
2015-08-27 13:05:12 -0500

    Touch up: application.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
 	
+  newGame();
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +14,104 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+    $("a.new").on('click', function(){
+      newGame();
+    });
+
+    $('input.button').on('click', function(){
+      // e.preventDefault();
+      var guess = $('#userGuess').val();
+      numOfAttempts += 1;
+      updateAttempt();
+      console.log("User guessed: " + guess);
+      comparing(guess, secNum);
+
+    });  
+
+
 });
+
+
+var numOfAttempts = 0;
+var secNum;
+var winner;
+
+function secretnumber(){
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function updateAttempt(){
+  $("#count").text(numOfAttempts);
+  console.log("Attempt: " + numOfAttempts);
+}
+
+function newGame(){
+  secNum = secretnumber();
+  console.log("The secret number is: " + secNum);
+  
+  numOfAttempts = 0;
+  updateAttempt();
+  $('#guessList').empty();
+  $('#userGuess').val('');
+
+
+  // $('input.button').on('click', function(e){
+  //   console.log("I'm running formSubmit")
+  //   e.preventDefault();
+  //   var guess = $('#userGuess').val();
+  //   numOfAttempts += 1;
+  //   updateAttempt();
+  //   console.log("User guessed: " + guess);
+  //   comparing(guess, secNum);
+
+  // });  
+}
+
+
+function comparing(inputGuess , inputSecret) {
+  var absoluteValue = (Math.abs(inputGuess - inputSecret));
+  if (inputSecret == inputGuess) {
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    winner = prompt("You guessed it! Shall we play again? Y/N").toUpperCase();
+    if(winner){
+      newGame();
+    }
+    $('#feedback').text("You won the game.");
+  } else if (absoluteValue > 50){
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    $('#feedback').text("You're too far away to give a hint...");
+  } else if (absoluteValue >= 30 && absoluteValue <= 50) {
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    $('#feedback').text("You're freezing cold. Try again.");
+  } else if (absoluteValue >= 20 && absoluteValue < 30) {
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    $('#feedback').text("You're close but still cold.");
+  } else if (absoluteValue >= 10 && absoluteValue < 20){
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    $('#feedback').text("You're warm!");
+  } else if (absoluteValue >= 1 && absoluteValue < 10){
+    $('#guessList').append('<li>' + inputGuess + '</li>');
+    $('#feedback').text("You're hot!");
+  }
+}
+
+
+
+
+
+var friends = ['M', 'A', 'S', 'R'];
+
+friends.forEach(function(a, b){
+  console.log(b + 1 + ". " + a);
+});
+
+
+
+
+
+
+
+
 
 
---
+++ js/application.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+  
+  /*--- Display information modal box ---*/
+    $(".what").click(function(){
+      $(".overlay").fadeIn(1000);
+
+    });
+
+  /*--- Hide information modal box ---*/
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
+
+  /*--- Starts a new game ---*/
+    $('.new').click(function(){
+      newGame();
+      $('#count').html(0);
+      $('#feedback').html("Make your Guess!");
+      $('#guessList').empty();
+    });
+
+    newGame();  
+
+    $('#guessButton').click(function(){
+      var guess = $('#userGuess').val();
+      console.log(typeof guess);
+      updateAttempt();
+      console.log("Attempt: " + attempt + "  Guess: " + guess);
+      compare(guess, num);
+      $('#userGuess').val("");
+      $('#count').html(attempt);
+      $('#guessList').append('<li>' + guess + '</li>');
+    });
+
+});
+
+var num;
+var attempt = 0;
+
+/*--- updates the num of times user has guessed ---*/
+var updateAttempt = function(){
+  attempt++
+};
+
+
+/*--- new game: new secret numer & reset attempt count ---*/
+var newGame = function(){
+  secretNum();
+  attempt = 0;
+};
+
+
+/*--- supplies the secret number ---*/
+var secretNum = function(){
+  num = Math.floor((Math.random() * 100) + 1);
+  console.log("Secret number: " + num);
+  return num
+};
+
+
+// var isNumber = function(input, secNum){
+//   if(Number(input) == NaN){
+//     $('#feedback').html("Numbers only!");
+//   } else {
+//     compare(input, secNum);
+//   }
+// };
+
+
+// input (guess)
+var compare = function(input, secNum){
+  if(input == secNum){
+    $('#feedback').html("Congrats! You win! Play again?");
+  } else if(Math.abs(input - secNum) <= 5 && (input != secNum)){
+    $('#feedback').html("5 or less...");
+  } else if(Math.abs(input - secNum) <= 10){
+    $('#feedback').html("10 or less...");
+  } else if(Math.abs(input - secNum) <= 20){
+    $('#feedback').html("20 or less...");
+  } else if(Math.abs(input - secNum) <= 30){
+    $('#feedback').html("30 or less...");
+  } 
+  else {
+    $('#feedback').html("You are no where near the number...")
+  }
+};
+
+
+// you will need to write code that ensures that the 
+// user has supplied a numeric input between 1 and 100.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
